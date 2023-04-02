b2152a66080fddc26e664951e24e8a8cd876f1e3
C_Caballero <carlopcaballero@gmail.com>
2023-04-02 01:15:39 -0400

    Organized Files. Setup start of scripting

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+var cityInput = $("#citySearch");
+var searchButton = $("#citySearchButton");
+var currentTemp = $("#currentTemp");
+var currentHumidity = $("#currentHumidity");
+var currentWind = $("#currentWindSpd");
+var currentUV = $("#currentUV");
+
+var APIKey = "72ba79407c82fcaf98978e68160d5733";
+
+function showWeather() {
+
+}
+
+searchButton.on("click",showWeather);
